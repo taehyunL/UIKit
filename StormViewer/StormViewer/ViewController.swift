9517e733c629063105b5e8b1aaa95d498e0ead4e
taehyunL <taylor1233@naver.com>
2022-08-14 21:42:53 +0900

    initial commit

---
+++ StormViewer/StormViewer/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by 이태현 on 2022/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
