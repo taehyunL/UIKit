f0d6eed2ae08b2924e44cb0be5de271a365fe0e0
taehyunL <taylor1233@naver.com>
2022-08-16 12:35:17 +0900

    navigation title 설정, 크기 까지

--- StormViewer/StormViewer/DetailViewController.swift
+++ StormViewer/StormViewer/DetailViewController.swift
@@ -9,14 +9,29 @@ import UIKit
 
 class DetailViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
         // Do any additional setup after loading the view.
+        if let selectedImage = selectedImage {
+            imageView.image = UIImage(named: selectedImage)
+        }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
     /*
     // MARK: - Navigation
 
