cf13fbe5ea438c7fe02a353f8e9e267b625397e4
taehyunL <taylor1233@naver.com>
2022-08-24 17:00:06 +0900

    initial commit

---
+++ WordScramble/WordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by 이태현 on 2022/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
