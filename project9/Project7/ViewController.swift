6e452683127dcdfd4bc2990bba19fd015d8f6a90
taehyunL <taylor1233@naver.com>
2022-09-08 15:22:50 +0900

    performSelector 활용 간편한 background, main thread 전환

--- project9/Project7/ViewController.swift
+++ project9/Project7/ViewController.swift
@@ -13,15 +13,21 @@ class ViewController: UITableViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+        
+        performSelector(inBackground: #selector(fetchJson), with: nil)
+        
+	}
 
-		let urlString: String
+    @objc func fetchJson() {
+        let urlString: String
 
-		if navigationController?.tabBarItem.tag == 0 {
-			urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
-		} else {
-			urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
-		}
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
 
+        
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
@@ -29,15 +35,17 @@ class ViewController: UITableViewController {
             }
         }
 
-        showError()
-	}
-
+        performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
+    }
+    
     func parse(json: Data) {
         let decoder = JSONDecoder()
 
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
-            tableView.reloadData()
+            tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+        } else {
+            performSelector(onMainThread: #selector(showError), with: nil, waitUntilDone: false)
         }
     }
 
@@ -61,10 +69,10 @@ class ViewController: UITableViewController {
 		navigationController?.pushViewController(vc, animated: true)
 	}
 
-	func showError() {
-		let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
-		ac.addAction(UIAlertAction(title: "OK", style: .default))
-		present(ac, animated: true)
+	@objc func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
 	}
 }
 
